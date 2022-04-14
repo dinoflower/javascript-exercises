de31d5cf313888a0ecb8732df03fa2fb67bcd734
dinoflower <kaedilove@gmail.com>
2022-04-14 16:35:32 -0500

    solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    array.reverse();
+    let outputString = array.join('');
+    return outputString;
 };
 
 // Do not edit below this line
