9bcf07baf88b612b19a12928a406919cf5023a4d
dinoflower <kaedilove@gmail.com>
2022-04-18 17:26:39 -0500

    pass first test, write alternative solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,38 @@
-const removeFromArray = function() {
+//const removeFromArray = function(array, ...values) {
+//  let index = array.indexOf(value);
+//  console.log(index);
+//  array.splice(index, 1);
+//  return array;
+//};
 
+//const removeFromArray = function(array, value) {
+//  function inArray() {
+//    for (let element of array) {
+//      if (element === value) {
+//        console.log(value);
+//        continue;
+//      }
+//      console.log(element);
+//      return element;
+//    }
+//  }
+//  let filtered = array.filter(inArray);
+//  return filtered;
+//}
+
+const removeFromArray = function (array, value) {
+  let newArray = [
+  ];
+  for (let element of array) {
+    if (element === value) {
+      continue;
+    }
+    else {
+      newArray.push(element);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
