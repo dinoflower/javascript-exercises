2d23ede31a4c7f86fcb385b3ed45803552e88ab8
dinoflower <kaedilove@gmail.com>
2022-04-19 13:47:56 -0500

    add conditional, remove alternate code from comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,28 +1,13 @@
 const removeFromArray = function(array, ...inputs) {
   for (let i = 0; i < inputs.length; i++) {
     let index = array.indexOf(inputs[i]);
-    console.log(index);
+    if (index < 0) {
+      continue;
+    }
     array.splice(index, 1);
   }
   return array;
 };
-
-// if the ...values are an array, need to access each item within the array in order to compare
-
-
-//const removeFromArray = function (array, ...inputs) {
-//  let newArray = [
-//  ];
-//  for (let element of array) {
-//    if (element === inputs[i]) {
-//      continue;
-//    }
-//    else {
-//      newArray.push(element);
-//    }
-//  }
-//  return newArray;
-//};
 
 // Do not edit below this line
 module.exports = removeFromArray;
