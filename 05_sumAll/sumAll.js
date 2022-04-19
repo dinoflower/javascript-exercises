8ab7f947845b3c49dd57ed451677bdc92fc3522e
dinoflower <kaedilove@gmail.com>
2022-04-19 14:30:38 -0500

    pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) {
+        return 'ERROR';
+    }
+    else if (Number(x) !== x || Number(y) !== y) {
+        return 'ERROR';
+    }
+    else {
+        let sum = 0;
+        if (x < y) {
+            while (x < y) {
+                sum += x;
+                x++;
+            }
+            sum += y;
+        }
+        else {
+            while (x > y) {
+                sum += x;
+                x--;
+            }
+            sum += y;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
