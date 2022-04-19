23f00912c8849c7173c042122b44f15cef98aa1b
dinoflower <kaedilove@gmail.com>
2022-04-19 15:26:16 -0500

    pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  let C = (F - 32) * (5 / 9);
+  if (Number.isInteger(C)) {
+    return C;
+  }
+  return Number(Number.parseFloat(C).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+  let F = C * (9 / 5) + 32;
+  if (Number.isInteger(F)) {
+    return F;
+  }
+  return Number(Number.parseFloat(F).toFixed(1));
 };
 
 // Do not edit below this line
