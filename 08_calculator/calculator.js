c763369a3b6b899e237445e23a3d311daf4ea5da
dinoflower <kaedilove@gmail.com>
2022-05-13 16:30:33 -0500

    solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const total = array.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const product = array.reduce(
+    (previousValue, currentValue) => previousValue * currentValue
+  );
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  return number < 2 ? 1 : number * factorial(number - 1);
 };
 
 // Do not edit below this line
