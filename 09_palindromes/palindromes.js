03cd6961ce4dde3e1817f86d2ffb4f87f97b12c5
dinoflower <kaedilove@gmail.com>
2022-05-15 16:26:26 -0500

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let temp = string.toLowerCase();
+    let newArray = Array.from(temp);
+    let filtered = newArray.filter(letter => /[a-z]/g.test(letter));
+    let newString = filtered.join('');
+    let reversedArray = filtered.reverse();
+    let reversedString = reversedArray.join('');
+    console.log(newString);
+    console.log(reversedString);
+    return newString === reversedString ? true : false;
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
