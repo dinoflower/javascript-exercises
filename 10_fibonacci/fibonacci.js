b2dd2ab7af06477694bcacd04aa0fc31db3c3c2d
dinoflower <kaedilove@gmail.com>
2022-05-16 14:59:58 -0500

    solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let sequence = [0, 1, 1];
+    Number(input);
+    if (input < 0) {
+        return "OOPS"
+    }
+    else {
+        for (let i = 2; i < input; i++) {
+            let value = sequence[i] + sequence[i-1];
+            sequence.push(value);
+        }
+        return sequence[input];
+    }
 };
+
+//cute but holy shit try an array
 
 // Do not edit below this line
 module.exports = fibonacci;
