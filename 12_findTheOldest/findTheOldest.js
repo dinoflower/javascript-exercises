520a81250fb1277058d75da5008e432fcc724d9a
dinoflower <kaedilove@gmail.com>
2022-05-16 16:15:08 -0500

    pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(a, b) {
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = (new Date()).getFullYear();
+            const last = a.yearOfDeath - a.yearOfBirth;
+            const next = b.yearOfDeath - b.yearOfBirth;
+            return last > next ? -1 : 1;
+        }
+        else if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = (new Date()).getFullYear();
+            const last = a.yearOfDeath - a.yearOfBirth;
+            const next = b.yearOfDeath - b.yearOfBirth;
+            return last > next ? -1 : 1;
+        }
+        else {
+        const last = a.yearOfDeath - a.yearOfBirth;
+        const next = b.yearOfDeath - b.yearOfBirth;
+        return last > next ? -1 : 1;
+        }
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
